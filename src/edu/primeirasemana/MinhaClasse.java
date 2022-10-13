50c4a9b2c8a4cb3a239e0032f3a600d16e2987da
exteferson <esteferson.quadros@gmail.com>
2022-10-13 00:56:24 -0300

    Comando com incremento ++ e decremento --.

--- src/edu/primeirasemana/MinhaClasse.java
+++ src/edu/primeirasemana/MinhaClasse.java
@@ -8,6 +8,13 @@ public static void main (String [] args){
     
     String nomeCompleto = juntarNomes(primeiroNome, segundoNome);
     System.out.println(nomeCompleto);
+
+//Testando incremento e Decremento:
+    int numero1 = 5, numero2 = 3;
+    numero1++;
+    numero2--;
+    System.out.println(numero1);
+    System.out.println(numero2);
 }
  public static String juntarNomes (String primeiroNome, String segundoNome){
     //return primeiroNome.concat(" ").concat(segundoNome);
